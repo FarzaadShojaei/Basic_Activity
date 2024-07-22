38617ed5426fb049e9703913a2c0e2f0776229cc
farzaadshojaei <farzaadshojaei@gmail.com>
2024-07-22 06:43:09 +0330

    Completing Flow Control

--- src/Flow_Control.kt
+++ src/Flow_Control.kt
@@ -1,6 +1,8 @@
 fun main(){
+
+    /*
 println("Input a boolean value. Is it night time?")
-    val input= readLine()
+    val input= readLine()?:""
     val night=input.toBoolean()
     if(night){
         println("Sleep, it's night time")
@@ -8,5 +10,80 @@ println("Input a boolean value. Is it night time?")
         println("Be active, It's day time")
     }
 
-    
+
+     */
+
+    /*
+    println("Please Input the Fund Amounts")
+    val input= readLine()?:""
+    var clientFunds=input.toInt();
+    var price=50
+    var clientProducts=0;
+
+    if(clientProducts> price){
+        clientFunds -=price;
+        clientProducts=1;
+        println("You have purchased $clientProducts ")
+        println("You have $clientFunds funds")
+    }else{
+        println("You have insufficient funds to purchase")
+    }
+
+
+     */
+    /*
+    // If Exercise
+val hasEggs=true
+val hasBacon=false
+val eggPrice=5
+    val baconPrice=20
+    var total=0
+
+    if(hasEggs){
+        total +=eggPrice *12
+        if(hasBacon){
+            total +=baconPrice *2
+        }else{
+            println("No Eggs available")
+        }
+        println("Total is $total")
+    }
+
+     */
+
+// Curly Brackets can be omitted if there is one experiment at the moment
+
+    /*
+    println("Input a number (1 to 100)")
+    val input = readLine()?:"0"
+    val number = input.toInt()
+    if(number > 100) {
+        println("Wrong number introduced")
+    } else {
+        val grade = if (number >= 90) {
+            "A"
+        } else if (number >= 80) {
+            "B"
+        } else if (number >= 70) {
+            "C"
+        } else if (number >= 60) {
+            "D"
+        } else if (number >= 50) {
+            "E"
+        } else {
+            "F"
+        }
+        println("You received grade $grade")
+    }
+
+     */
+
+/*
+    val number = 24
+    val isEven = if(number % 2 == 0) "Number is even" else "Number is odd"
+
+    println(isEven)
+
+ */
+
 }
