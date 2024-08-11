2dee33cded3cb999e3d9f86e4514799acccead45
farzaadshojaei <farzaadshojaei@gmail.com>
2024-08-11 23:58:24 +0330

    adding companion practice

---
+++ src/Init_Solution.kt
@@ -0,0 +1,13 @@
+fun main(){
+    val browser=WebBrowser()
+}
+
+
+class WebBrowser{
+    init {
+        println("Web browser is connecting")
+        println("http://www.google.com/")
+    }
+
+
+}
