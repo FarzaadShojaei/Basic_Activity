8ead3bf488440ab9b3e24bcaef7a9c2b462e66fd
farzaadshojaei <farzaadshojaei@gmail.com>
2024-08-15 23:15:38 +0330

    Adding Let and With

--- src/Inheritance_Practice.kt
+++ src/Inheritance_Practice.kt
@@ -1,11 +1,11 @@
 fun main(){
 
-val myLaptop=Laptop()
+val myLaptop=Laptop("Windows 10")
 val myApple=Apple()
 }
 
 
-open class Laptop{
+open class Laptop(s: String) {
     var screenSize=15
     var speed=1200
     var name="Generic Laptop"
@@ -15,4 +15,4 @@ open class Laptop{
     }
 }
 
-class Apple:Laptop(){}
+class Apple:Laptop("Windows 10") {}
