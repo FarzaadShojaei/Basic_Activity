7adf900e6b988792dd8b0d89165fc7cb213137f2
farzaadshojaei <farzaadshojaei@gmail.com>
2024-08-16 22:06:20 +0330

    AddingAlso,Apply,With

--- src/Apply_Solution.kt
+++ src/Apply_Solution.kt
@@ -1,6 +1,25 @@
 fun main(){
-
+println("What is your name")
+    val name= readLine()?:""
+    CoffeeShop().sellCoffee(name)
 
 
 }
+class CoffeeShop{
+    fun sellCoffee(name:String){
+    CoffeeCup().apply {
+    clientName=name
+        prepareCoffee()
+        println("Serving coffee to client $clientName")
+        println(this)
+    }
 
+    }
+}
+
+class CoffeeCup{
+    var clientName=""
+    fun prepareCoffee(){
+        println("Brewing the coffee")
+    }
+}
--- src/With_Solution.kt
+++ src/With_Solution.kt
@@ -1,5 +1,21 @@
 fun main(){
+with(Store1()){
+    shoes = 25
+    shirts=10
+    jackets=43
+    printInventory()
+}
 
 
+}
 
+
+class Store1{
+    var shoes=10
+    var shirts=20
+    var jackets= 30
+
+    fun printInventory(){
+        println("The Store has $shoes pairs of shoes, $shirts shirts and $jackets jackets")
+    }
 }
